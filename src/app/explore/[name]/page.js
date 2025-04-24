4f393ef76d6f1615b3dd8537ca6a4cfb753ad98a
keshavbansal5766 <keshav22.dlp@gmail.com>
2025-04-24 22:42:42 +0530

    create restaurants details api

--- src/app/explore/[name]/page.js
+++ src/app/explore/[name]/page.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const page = async ({ params }) => {
   const { name } = await params;
+
+  useEffect(() => {
+    loadFoodItem();
+  }, []);
+
+  const loadFoodItem = async (id) => {
+    let response = await fetch("http://localhost:3000/api/customer/" + id);
+  };
 
   return (
     <div className="restaurant-page-banner">

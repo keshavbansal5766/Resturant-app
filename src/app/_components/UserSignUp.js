e2de40cd7b99acb7dca7018e7536e499f69aa114
keshavbansal5766 <keshav22.dlp@gmail.com>
2025-04-26 02:20:11 +0530

    Create userSignup api

--- src/app/_components/UserSignUp.js
+++ src/app/_components/UserSignUp.js
@@ -10,8 +10,21 @@ const UserSignUp = () => {
   const [address, setAddress] = useState("");
   const [contact, setContact] = useState("");
 
-  const handleSignUp = () => {
+  const handleSignUp = async () => {
     console.log(name, email, password, confirmPassword, city, address, contact);
+
+    let response = await fetch("http://localhost:3000/api/user", {
+      method: "POST",
+      body: JSON.stringify({ name, email, password, city, address, contact }),
+    });
+
+    response = await response.json();
+
+    if (response.success) {
+      alert("User created");
+    } else {
+      alert("failed");
+    }
 
     setName("");
     setEmail("");
